f7229390a7da7047d63a155a9d028c6185f7d6f7
Carla Oliveira <89038203+cajuliana@users.noreply.github.com>
2021-09-29 13:18:39 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,44 @@
+//ex 1
+
+console.log(!true)
+
+console.log(!false)
+
+console.log(!!false)
+
+console.log(!!true)
+
+console.log(!1)
+
+console.log(!!1)
+
+console.log(!0)
+
+console.log(!!0)
+
+console.log(!!"")
+
+console.log(5 < 10 && 5 !== 5)
+
+console.log(5 > 9 || 5 === 5)
+
+console.log(!(5===9))
+
+//ex 2
+
+console.log("-----------exercicio2---------")
+
+let x = 10
+let y = "a"
+
+console.log(y==="b" || x >= 10)
+
+let a = 3
+let b = 8
+
+console.log(!(a == "3" || a === b) && !(b !== 8 && a <= b))
+
+let str = ""
+let msg = "haha!"
+let eBonito = "false"
+console.log(!((str || msg) && eBonito))
